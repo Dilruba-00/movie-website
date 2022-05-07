820c82fb4b817f5d335b097fdb557783f809be98
Dilruba Tunçez <dirruba_2000@hotmail.com>
2022-05-07 21:16:08 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ arrows.forEach((arrow, i) => {
        clickCounter++;
        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
         movieLists[i].style.transform = `translateX(${ 
-            movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`;
+            movieLists[i].computedStyleMap().get("transform")[0].x.value - 315}px)`;
        } else {
            movieLists[i].style.transform = "translateX(0)";
            clickCounter = 0;
